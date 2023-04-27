b9cb4947618885d70a05f322f2ea9908da9ba8fe
Miao Wang <shankerwangmiao@gmail.com>
2023-04-27 10:26:31 +0800

    replace help with zhelp

--- _plugins/select.rb
+++
@@ -1,9 +0,0 @@
-require "rubygems"
-require "json"
-
-module SelectFilter
-def select(input, *selected)
-    input.map{ |o| o.to_liquid.hash_for_json.reject! { |k| not selected.include? k } }
-end
-Liquid::Template.register_filter self
-end
